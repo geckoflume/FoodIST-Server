c698d3543217bf59dccfe6986fa851d5f58d5be3
Florian Mornet <Florian.Mornet@bordeaux-inp.fr>
2020-05-05 09:53:01 +0200

    Removed full url from filename
    Signed-off-by: Florian Mornet <Florian.Mornet@bordeaux-inp.fr>

--- utils/PictureUploader.php
+++ utils/PictureUploader.php
@@ -4,12 +4,10 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class PictureUploader
 {
-    private $baseUrl;
     private $newFilename;
 
     function __construct(UploadedFile $uploadedPicture)
     {
-        $this->baseUrl = 'https://' . $_SERVER['SERVER_NAME'] . '/uploads/';
         $destination = __DIR__ . '/../uploads';
         $this->newFilename = uniqid() . '.' . $uploadedPicture->guessExtension();
         $uploadedPicture->move($destination, $this->newFilename);
@@ -17,6 +15,6 @@ class PictureUploader
 
     function getNewFilename()
     {
-        return $this->baseUrl . $this->newFilename;
+        return $this->newFilename;
     }
 }
